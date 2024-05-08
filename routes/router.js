2f70af3e667b47e944929394c1c7e9487a17a2aa
Karthik <karthikbs@xminds.com>
2024-05-08 12:04:05 +0530

    change in router

--- routes/router.js
+++ routes/router.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const services = require('../modules/services');
+const { getAllBooks } = require('../modules/services');
+const { createBook } = require('../modules/services');
+const { updateBook } = require('../modules/services');
+const { deleteBook } = require('../modules/services');
 
 router.use(express.json());
 
-router.get('/get', async (req, res) => {
-    services.getAllBooks(req, res);
-});
+router.get('/get', getAllBooks);
 
-router.post('/add', async (req, res) => {
-    services.createBook(req, res);
-});
+router.post('/add', createBook)
 
-router.put('/update/:id', async (req, res) => {
-    services.updateBook(req, res);
-});
+router.put('/update/:id', updateBook)
 
-router.delete('/delete/:id', async (req, res) => {
-    services.deleteBook(req, res);
-  });
+router.delete('/delete/:id', deleteBook)
 
 module.exports = router;
